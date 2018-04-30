7b78f7fb7bfcc07790db8d075f4d404fd97aa1bd
Kyle Burt <k1burt@ucsd.edu>
2018-04-29 23:03:16 -0700

    before pull

--- create_db.js
+++ create_db.js
@@ -1,6 +1,6 @@
 //to create a database
 
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('users.db');
 
 db.serialize(() => {
@@ -40,23 +40,33 @@ db.serialize(() => {
     }
   });
   //table of messages sent to users
+  /**
   db.run("CREATE TABLE message_to_user(messageId INTEGER, receiverId INTEGER, FOREIGN KEY messageId REFERENCES messages(messageId), FOREIGN KEY receiverId REFERENCES users_account(userId), PRIMARY KEY(messageId, receiverId)))", (err, row) => {
     if(err) {
       console.log("seventh");
     }
   });
-
+  */
   //primary key of table
   var id;
   //insert 2 rows of data using sql codes:
   db.run("BEGIN TRANSACTION");
   db.run("INSERT INTO users_account(firstName, email, lastName, isDeveloper) VALUES('Kyle', 'k1burt@ucsd.edu', 'Burt', '0')", function(err, row) {
     //get id of inserted user
-    id = this.lastID;
+    if (err) {
+      console.log("error");
+    }
+    id = this.lastID + '';
+    console.log(id);
   });  
-  db.run("INSERT INTO projects(projectTitle, projectDescription, userId) VALUES('Blue House', 'This is a blue house'," + id + ")")
-  db.run("END TRANSACTION;")
+  db.run("INSERT INTO projects(projectTitle, projectDescription, userId) VALUES('Blue House', 'This is a blue house'," + id + ")", (err, row) => {
+    if (err) {
+      console.log("aaaa");
+    }
+  });
+  db.run("END TRANSACTION;");
   
+  /**
   //checking if this works
   db.each("SELECT userId, firstName, lastName, email, isDeveloper FROM users_account", (err,row) => {
     console.log(row.userId + " " + row.firstName + " " + row.lastName + ":" + row.email + '.');
@@ -65,6 +75,7 @@ db.serialize(() => {
   db.each("SELECT projectId, projectTitle, projectDescription, userId FROM projects", (err, row) => {
     console.log(row.projectId + " " + row.projectTitle + " " + row.projectDescription + " " +row.userId);
   });
+  */
   
   /**
   db.run("BEGIN TRANSACTION");
