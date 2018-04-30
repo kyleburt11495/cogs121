e0fdeb4c29ced2ef70c6aa75e434c8409d7d1eb0
Kyle Burt <k1burt@ucsd.edu>
2018-04-29 22:23:17 -0700

    error in last create table

--- create_db.js
+++ create_db.js
@@ -5,31 +5,30 @@ const db = new sqlite3.Database('users.db');
 
 db.serialize(() => {
   //user table
-  db.run("CREATE TABLE users_account (userId INTEGER PRIMARY KEY, firstName NOT NULL TEXT, email NOT NULL TEXT, lastName NOT NULL TEXT, isDeveloper NOT NULL INTEGER)", (err, row) => {
+  db.run("CREATE TABLE users_account (userId INTEGER PRIMARY KEY, firstName TEXT, email TEXT, lastName TEXT, isDeveloper INTEGER)", (err, row) => {
     if(err) {
       console.log("first");
     }
   });
-  //project table
-  db.run("CREATE TABLE projects(projectId INTEGER PRIMARY KEY, projectTitle NOT NULL TEXT, projectDescription TEXT, userId NOT NULL INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId))", (err, row) => {
+  db.run("CREATE TABLE projects(projectId INTEGER PRIMARY KEY, projectTitle TEXT, projectDescription TEXT, userId INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId))", (err, row) => {
     if(err) {
       console.log("seocnd");
     }
   });
   //picture table
-  db.run("CREATE TABLE pictures(pictureId INTEGER PRIMARY KEY, picture NOT NULL BLOB, projectId NOT NULL INTEGER, FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
+  db.run("CREATE TABLE pictures(pictureId INTEGER PRIMARY KEY, picture BLOB, projectId INTEGER, FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
     if(err) {
       console.log("third");
     }
   });
   //table of projects followed by users
-  db.run("CREATE TABLE following_projects(followRelationshipId INTEGER PRIMARY KEY, userId NOT NULL INTEGER, projectId NOT NULL INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId), FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
+  db.run("CREATE TABLE following_projects(followRelationshipId INTEGER PRIMARY KEY, userId INTEGER, projectId INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId), FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
     if(err) {
       console.log("fourth");
     }
   });
   //table of projects created by users
-  db.run("CREATE TABLE created_projects(createdRelationshipId INTEGER PRIMARY KEY, userId NOT NULL INTEGER, projectId NOT NULL INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId), FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
+  db.run("CREATE TABLE created_projects(createdRelationshipId INTEGER PRIMARY KEY, userId INTEGER, projectId INTEGER, FOREIGN KEY(userId) REFERENCES users_account(userId), FOREIGN KEY(projectId) REFERENCES projects(projectId))", (err, row) => {
     if(err) {
       console.log("fifth");
     }
@@ -41,7 +40,7 @@ db.serialize(() => {
     }
   });
   //table of messages sent to users
-  db.run("CREATE TABLE message_to_user(messageId INTEGER, receiverId INTEGER, FOREIGN KEY messageId REFERENCES messages(messageId, FOREIGN KEY receiverId REFERENCES users_account(userId), PRIMARY KEY(messageId, receiverId)))", (err, row) => {
+  db.run("CREATE TABLE message_to_user(messageId INTEGER, receiverId INTEGER, FOREIGN KEY messageId REFERENCES messages(messageId), FOREIGN KEY receiverId REFERENCES users_account(userId), PRIMARY KEY(messageId, receiverId)))", (err, row) => {
     if(err) {
       console.log("seventh");
     }
