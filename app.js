78bcb5a132d5c994b6a0cb7bb4f4eae4a2e1abc7
Vincent Tierra <vtierra@ucsd.edu>
2018-05-11 14:20:56 -0700

    minor change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const db = new sqlite3.Database('users.db');
 
 app.use(express.static('static_files'));
 //to insert new accounts
-app.use(bodyParser.urlencoded({extented: true})); //hook to the app
+app.use(bodyParser.urlencoded({extended: true})); //hook to the app
 
 app.listen(3000, () => {
   console.log('Server started');
@@ -46,41 +46,41 @@ app.get('/upload', (req, res) => {
 
 //Simulate database in memory
 //Trending, popular, following
-const trending = {
-  img1:'interior_design1.jpg', img2:'interior_design2.jpg', img3:'interior_design3.jpg', img4:'interior_design5.jpg'
-};
-
-const popular = {
-  img1:'interior_design4.jpg', img2:'interior_design5.jpg', img3:'interior_design6.jpg', img4: 'interior_design2.jpg'
-};
-
-const following = {
-  img1:'interior_design7.jpg', img2:'interior_design8.jpg', img3: 'interior_design9.jpg', img4: 'interior_design6.jpg'
-};
-
-const trendingStories = {
-  img1:'story1.jpg', img2:'story2.jpg', img3:'story3.jpg', img4:'story4.jpg',
-  img5:'story5.jpg'
-};
-
-const popularStories = {
-  img1:'story3.jpg', img2:'story7.jpg', img3:'story8.jpg', img4:'story9.jpg',
-  img5:'story2.jpg'
-};
-
-const followingStories = {
-  img1:'story4.jpg', img2:'story1.jpg', img3:'story8.jpg', img4:'story6.jpg',
-  img5:'story5.jpg'
-};
-
-const database = {
-  trendingProjects: trending,
-  popularProjects: popular,
-  followingProjects: following,
-  trendingStories: trendingStories,
-  popularStories: popularStories,
-  followingStories: followingStories
-};
+// const trending = {
+//   img1:'interior_design1.jpg', img2:'interior_design2.jpg', img3:'interior_design3.jpg', img4:'interior_design5.jpg'
+// };
+//
+// const popular = {
+//   img1:'interior_design4.jpg', img2:'interior_design5.jpg', img3:'interior_design6.jpg', img4: 'interior_design2.jpg'
+// };
+//
+// const following = {
+//   img1:'interior_design7.jpg', img2:'interior_design8.jpg', img3: 'interior_design9.jpg', img4: 'interior_design6.jpg'
+// };
+//
+// const trendingStories = {
+//   img1:'story1.jpg', img2:'story2.jpg', img3:'story3.jpg', img4:'story4.jpg',
+//   img5:'story5.jpg'
+// };
+//
+// const popularStories = {
+//   img1:'story3.jpg', img2:'story7.jpg', img3:'story8.jpg', img4:'story9.jpg',
+//   img5:'story2.jpg'
+// };
+//
+// const followingStories = {
+//   img1:'story4.jpg', img2:'story1.jpg', img3:'story8.jpg', img4:'story6.jpg',
+//   img5:'story5.jpg'
+// };
+//
+// const database = {
+//   trendingProjects: trending,
+//   popularProjects: popular,
+//   followingProjects: following,
+//   trendingStories: trendingStories,
+//   popularStories: popularStories,
+//   followingStories: followingStories
+// };
 
 app.get('/trending', (req, res) => {
   db.all("SELECT * FROM projects WHERE isTrending = 1", (err, row) => {
@@ -187,10 +187,10 @@ app.post('/signup', (req, res)=>{
         return match;
       }
     })
-    console.log('it exist', match);
+    console.log('it exists', match);
     // console.log(match);
     if(match){
-      console.log('user already exist');
+      console.log('user already exists');
       // res.send({});
       return;
     }
@@ -218,45 +218,4 @@ app.post('/signup', (req, res)=>{
       );
     }
   });
- //  else{
- //    // let match = false;
- //    // db.all("SELECT email FROM users_account", (err,row)=>{
- //    //     row.forEach((e)=> {
- //    //     if(e.email == req.body.email){
- //    //       console.log("match");
- //    //       res.send({});
- //    //
- //    //     }
- //    //     };
- //    //   /*if(req.body.email == row.email){
- //    //     console.log("match");
- //    //     return false;
- //    //   } */
- //    //   if(match){
- //    //     res.send({});
- //    //   } else {
- //    db.run(
- //      'INSERT INTO users_account(firstName, email, lastName, isDesigner, password) VALUES ($firstName, $email, $lastName, $isDesigner, $password)',
- //      {
- //        $firstName: req.body.firstName,
- //        $email: req.body.email,
- //        $lastName: req.body.lastName,
- //        $isDesigner: req.body.isDesigner,
- //        $password: req.body.password,
- //      },
- //      (err) => {
- //        if(err) {
- //          console.log('error creating new user');
- //        } else{
- //          console.log("hi");
- //          db.each("SELECT userId, firstName, email, isDesigner FROM users_account", (err,row)=>{
- //            console.log(row.userId + " " + row.firstName + ":" + row.email + '.');
- //          });
- //          res.send({message:'successfuly run app.post(/signup)'});
- //        }
- //      }
- //    );
- // }
-//  }
-//  });
 });
